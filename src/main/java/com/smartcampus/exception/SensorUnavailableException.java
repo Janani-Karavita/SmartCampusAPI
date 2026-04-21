cb568296cb88f3e0556340c2b83e7d595f2f96a6
Jaanu-17 <jananinavindula2@gmail.com>
2026-04-21 13:30:36 +0530

    sensor unavailable exception

---
+++ src/main/java/com/smartcampus/exception/SensorUnavailableException.java
@@ -0,0 +1,11 @@
+package com.smartcampus.exception;
+
+/**
+ * Exception thrown when a sensor is unavailable.
+ */
+public class SensorUnavailableException extends ApiException {
+
+    public SensorUnavailableException(String message) {
+        super("FORBIDDEN", message);
+    }
+}
