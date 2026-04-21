56e05c00cf50f605175007f401db421e54387fd3
Jaanu-17 <jananinavindula2@gmail.com>
2026-04-21 13:29:23 +0530

    bad request exception

---
+++ src/main/java/com/smartcampus/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.smartcampus.exception;
+
+public class BadRequestException extends ApiException {
+    public BadRequestException(String message) {
+        super("BAD_REQUEST", message);
+    }
+}
